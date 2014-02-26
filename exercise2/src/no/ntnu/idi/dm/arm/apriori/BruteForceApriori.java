4d906a2b576c23ed2ef31dca0ea7cecdafd88971
Kristian Snare <kristian.snare@outlook.com>
2014-02-26 08:11:25 +0100

    Brute force

--- exercise2/src/no/ntnu/idi/dm/arm/apriori/BruteForceApriori.java
+++ exercise2/src/no/ntnu/idi/dm/arm/apriori/BruteForceApriori.java
@@ -10,7 +10,36 @@ public class BruteForceApriori<V> extends BaseApriori<V> {
 
 	@Override
 	public void apriori(Double minSupport) {
-		// TODO
-	}
+		// get 1-itemsets
+		List<ItemSet<V>> candidates = getAllItemsetsOfSizeOne();
+		System.out.println("Level 1:");
+		System.out.println(candidates.size()+ " total, unique itemsets: "+ candidates.size());
+		System.out.println("\t" + candidates);
 
+		// number of candidates on lowest level
+		int levels = candidates.size();
+
+		// store level 1 candidates in our list of frequent itemsets
+		frequentItemSets.put(1, candidates);
+
+		// create the higher levels
+		for (int i = 2;i<levels; i++) {
+			System.out.println("Level " + i);
+
+			// generate all sets
+			candidates=aprioriGen(candidates);
+			
+			
+			frequentItemSets.put(i, candidates);
+			
+			if (candidates.size() == 0) {
+				System.out.println("We're done here, there's no more frequent itemsets");
+				break;
+			}
+
+			System.out.println("\t" + candidates);
+		}
+
+	}
+	
 }
