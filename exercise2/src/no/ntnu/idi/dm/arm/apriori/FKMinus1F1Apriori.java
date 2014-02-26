24d895808634026f25f43bcbac723565898c2a93
Audun Skjervold <audun.xetrov@gmail.com>
2014-02-26 05:36:04 +0100

    Completed FKMinus1 x F1

--- exercise2/src/no/ntnu/idi/dm/arm/apriori/FKMinus1F1Apriori.java
+++ exercise2/src/no/ntnu/idi/dm/arm/apriori/FKMinus1F1Apriori.java
@@ -13,13 +13,30 @@ public class FKMinus1F1Apriori<V> extends BaseApriori<V> {
 	}
 
 	@Override
-	public List<ItemSet<V>> aprioriGen(
-			List<ItemSet<V>> frequentCandidatesKMinus1) {
+	public List<ItemSet<V>> aprioriGen(List<ItemSet<V>> frequentCandidatesKMinus1) {
 		Collections.sort(frequentCandidatesKMinus1);
 		int allGeneratedCandidatesCounter = 0;
 		Set<ItemSet<V>> frequentCandidateSet = new HashSet<ItemSet<V>>();
 		
-		//TODO
+		for (ItemSet<V> frequentCandidatePrevLevel : frequentCandidatesKMinus1) {
+			// Take each of the frequentCandidates from the previous level ... 
+			
+			for (ItemSet<V> frequentItem : frequent1Itemsets) {
+				// ... and get each of the frequent single items.
+				
+				if (frequentCandidatePrevLevel.intersection(frequentItem).size() == 0) {
+					// Does the set already contain this item? No?
+					
+					ItemSet<V> frequentCandidate = frequentCandidatePrevLevel.union(frequentItem);
+					// Then go ahead and combine them!
+					
+					frequentCandidateSet.add(frequentCandidate); // Make sure you add it to your set of candidates!
+					allGeneratedCandidatesCounter++; // Don't forget to count it!
+					getAndCacheSupportForItemset(frequentCandidate); // Oh, and it has some support values you'll need.
+				}
+				
+			}
+		}
 		
 		return new LinkedList<ItemSet<V>>(frequentCandidateSet);
 	}
