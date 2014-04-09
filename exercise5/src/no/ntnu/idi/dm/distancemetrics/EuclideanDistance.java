f37358a6f92357e52d051d320a5ca9beab2409e3
Audun Skjervold <audun.xetrov@gmail.com>
2014-04-09 21:55:46 +0200

    Added files for exercise 5

---
+++ exercise5/src/no/ntnu/idi/dm/distancemetrics/EuclideanDistance.java
@@ -0,0 +1,15 @@
+package no.ntnu.idi.dm.distancemetrics;
+
+/**
+ * Implements the L2 or Euclidean metric.
+ * 
+ */
+public class EuclideanDistance {
+
+	public double distance(double[] vector1, double[] vector2) {
+		double dist = 0;
+		//TODO
+		return Math.sqrt(dist);
+	}
+
+}
